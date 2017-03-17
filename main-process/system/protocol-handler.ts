bdaa1bbc47525e6a9285bb72605158c5de1cfc7d
Baptiste Lepilleur <baptiste.lepilleur@gmail.com>
2017-03-17 09:15:03 +0100

    Migrated protocol-handler.js to typescript

--- main-process/system/protocol-handler.ts
+++ main-process/system/protocol-handler.ts
@@ -1,8 +1,7 @@
-const app = require('electron').app
-const dialog = require('electron').dialog
+import {app, dialog} from 'electron'
 
 app.setAsDefaultProtocolClient('electron-api-demos')
 
-app.on('open-url', function (event, url) {
+app.on('open-url', function (event: Electron.Event, url: string): void {
   dialog.showErrorBox('Welcome Back', `You arrived from: ${url}`)
 })
